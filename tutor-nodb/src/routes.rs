afc4fe514abd8bb1a7b732f6335ba78688ab3d9a
Edanriell <Edanriell@gmail.com>
2025-08-18 12:01:46 +0300

    feature(get-all-courses): implemented route handler and test

--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -10,5 +10,8 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
 pub fn course_routes(cfg: &mut web::ServiceConfig) {
     cfg
         .service(web::scope("/courses")
-            .route("/", web::post().to(new_course)));
-}
+            .route("/", web::post().to(new_course))
+            // Add a new route for getting courses for a
+            // tutor (represented by the tutor_id variable).
+            .route("/{tutor_id}", web::get().to(get_courses_for_tutor)));
+} 
