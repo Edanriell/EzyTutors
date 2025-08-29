1f48cd2c4cb439bf4f44b9fdadccfdd72ae29795
Edanriell <Edanriell@gmail.com>
2025-08-29 19:23:19 +0300

    feature(error-handling): added error handling in db_access

--- tutor-db/src/db_access.rs
+++ tutor-db/src/db_access.rs
@@ -1,7 +1,16 @@
 use super::models::Course;
+use super::errors::EzyTutorError;
 use sqlx::postgres::PgPool;
 
-pub async fn get_courses_for_tutor_db(pool: &PgPool, tutor_id: i32) -> Vec<Course> {
+pub async fn get_courses_for_tutor_db(
+    pool: &PgPool,
+    tutor_id: i32
+// ) -> Vec<Course> {
+    // The function returns a Result<T> type
+    // representing two possible outcomes:
+    // Vec<Course> in the case of success, or
+    // the EzyTutorError error type on failure.
+) -> Result<Vec<Course>, EzyTutorError> {
     // Prepare the SQL statement for
     // retrieving query results using the
     // query! macro from the sqlx crate.
@@ -10,11 +19,25 @@ pub async fn get_courses_for_tutor_db(pool: &PgPool, tutor_id: i32) -> Vec<Cours
         tutor_id)
         // Execute the query.
         .fetch_all(pool)
-        .await
-        .unwrap();
+        // Replace await.unwrap() with await?. This converts
+        // the sqlx error to an EzyTutorError and
+        // propagates it to the calling web handler function.
+        .await?;
+        // .unwrap();
     // Convert the query results into a Rust vector,
     // which is returned from the function.
-    course_rows
+    // course_rows
+    //     .iter()
+    //     .map(|course_row| Course {
+    //         course_id: course_row.course_id,
+    //         tutor_id: course_row.tutor_id,
+    //         course_name: course_row.course_name.clone(),
+    //         posted_time: Some(chrono::NaiveDateTime::from(
+    //             course_row.posted_time.unwrap())),
+    //     })
+    //     .collect()
+
+    let courses: Vec<Course> = course_rows
         .iter()
         .map(|course_row| Course {
             course_id: course_row.course_id,
@@ -23,7 +46,15 @@ pub async fn get_courses_for_tutor_db(pool: &PgPool, tutor_id: i32) -> Vec<Cours
             posted_time: Some(chrono::NaiveDateTime::from(
                 course_row.posted_time.unwrap())),
         })
-        .collect()
+        .collect();
+    // If there are no query results for the tutor_id, return an error of
+    // type EzyTutorError, which will generate a message for the user
+    match courses.len() {
+        0 => Err(EzyTutorError::NotFound(
+            "Courses not found for tutor".into(),
+        )),
+        _ => Ok(courses),
+    }
 }
 
 pub async fn get_course_details_db(pool: &PgPool, tutor_id: i32, course_id: i32) -> Course {
