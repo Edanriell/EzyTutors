9438bc1d2d2ff4235b89c8b60dcb0e40ca43165b
Edanriell <Edanriell@gmail.com>
2025-10-12 01:02:26 +0300

    feature(tutor-entity): created

--- tutor-db/src/models/tutor.rs
+++ tutor-db/src/models/tutor.rs
@@ -1,12 +1,41 @@
-// he From trait will extract the data payload sent with the POST HTTP
-// request (for a new course) and convert it into the Rust Course data structure.
-impl From<web::Json<Course>> for Course {
-    fn from(course: web::Json<Course>) -> Self {
-        Course {
-            course_id: course.course_id,
-            tutor_id: course.tutor_id,
-            course_name: course.course_name.clone(),
-            posted_time: course.posted_time,
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Tutor {
+    tutor_id: i32,
+    tutor_name: String,
+    tutor_pic_url: String,
+    tutor_profile: String
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct NewTutor {
+    pub tutor_name: String,
+    pub tutor_pic_url: String,
+    pub tutor_profile: String
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct UpdateTutor {
+    pub tutor_name: Option<String>,
+    pub tutor_pic_url: Option<String>,
+    pub tutor_profile: Option<String>
+}
+
+impl From<web::Json<NewTutor>> for NewTutor {
+    fn from(new_tutor: web::Json<NewTutor>) -> Self {
+        NewTutor {
+            tutor_name: new_tutor.tutor_name.clone(),
+            tutor_pic_url: new_tutor.tutor_pic_url.clone(),
+            tutor_profile: new_tutor.tutor_profile.clone()
+        }
+    }
+}
+
+impl From<web::Json<UpdateTutor>> for UpdateTutor {
+    fn from(update_tutor: web::Json<UpdateTutor>) -> Self {
+        UpdateTutor {
+            tutor_name: update_tutor.tutor_name.clone(),
+            tutor_pic_url: new_tutor.tutor_pic_url.clone(),
+            tutor_profile: new_tutor.tutor_profile.clone()
         }
     }
 }
